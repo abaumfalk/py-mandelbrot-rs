92ce8772b33d5cbaf25f1401a8b05a3b2635c2b7
Arno Baumfalk <a.baumfalk@web.de>
2026-02-08 14:36:45 +0100

    initial mandelbrot calculation

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import argparse
 
 
+def mandelbrot_iterate(c, max_iter):
+    z = 0
+    for i in range(max_iter):
+        z = z * z + c
+        if abs(z) > 2:
+            return i
+    return max_iter
+
+
 def calculate_mandelbrot(x_min, x_max, y_min, y_max, width, height, iterations):
-    raise NotImplementedError
+    x_step = (x_max - x_min) / width
+    y_step = (y_max - y_min) / height
+
+    result = []
+    for y in range(height):
+        row = []
+        for x in range(width):
+            c = complex(x_min + x * x_step, y_min + y * y_step)
+            row.append(mandelbrot_iterate(c, iterations))
+        result.append(row)
+
+    return result
 
 
 if __name__ == "__main__":
@@ -14,5 +34,5 @@ if __name__ == "__main__":
     parser.add_argument("--height", type=int, help="Height of image", default=1000)
     args = parser.parse_args()
 
-    set = calculate_mandelbrot(args.range[0], args.range[1], args.range[2], args.range[3],
+    mandelbrot = calculate_mandelbrot(args.range[0], args.range[1], args.range[2], args.range[3],
                                args.width, args.height, args.iterations)

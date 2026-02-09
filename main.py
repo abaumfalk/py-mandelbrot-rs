091f61ca507608915f8355430706395dec6eb9e5
Arno Baumfalk <a.baumfalk@web.de>
2026-02-09 14:27:15 +0100

    use numpy array rather than lists (slower!)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import time
 import timeit
 from matplotlib import pyplot
-
+import numpy
 
 def mandelbrot_iterate(c, max_iter):
     z = 0
@@ -17,13 +17,12 @@ def calculate_mandelbrot(x_min, x_max, y_min, y_max, width, height, iterations):
     x_step = (x_max - x_min) / width
     y_step = (y_max - y_min) / height
 
-    result = []
+    result = numpy.empty((height, width))
+
     for y in range(height):
-        row = []
         for x in range(width):
             c = complex(x_min + x * x_step, y_min + y * y_step)
-            row.append(mandelbrot_iterate(c, iterations))
-        result.append(row)
+            result[y][x] = mandelbrot_iterate(c, iterations)
 
     return result
 

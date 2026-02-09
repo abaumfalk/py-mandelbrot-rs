a8fb6ec59d9122d3aab3b9cd83976ecc61d3dd8a
Arno Baumfalk <a.baumfalk@web.de>
2026-02-09 16:50:42 +0100

    paralellization -> slowdown ??

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ use pyo3::prelude::*;
 mod py_mandelbrot_rs {
     use pyo3::prelude::*;
     use num::Complex;
-
+    use rayon::prelude::*;
+    
     fn mandelbrot_iterate(cx: f64, cy: f64, max_iter: u32) -> u32 {
         let c = Complex::new(cx, cy);
         let mut z = Complex::new(0.0, 0.0);
@@ -25,7 +26,7 @@ mod py_mandelbrot_rs {
         let x_values: Vec<f64> = (0..width).map(|x| x as f64 * (x_max - x_min) / width as f64).collect();
         let y_values :Vec<f64> = (0..height).map(|y| y as f64 * (y_max - y_min) / height as f64).collect();
 
-        (0..height).map(|y| 
+        (0..height).into_par_iter().map(|y| 
             (0..width).map(|x| 
                 mandelbrot_iterate(x_values[x], y_values[y], max_iter)).collect()).collect()
     }

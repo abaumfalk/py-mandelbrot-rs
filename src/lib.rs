720eade9cb59b9e42a44d1d4278717bed1215900
Arno Baumfalk <a.baumfalk@web.de>
2026-03-05 16:21:24 +0100

    precalculate x/y values in rust code (no speedup)

--- src/lib.rs
+++ src/lib.rs
@@ -19,19 +19,25 @@ mod py_mandelbrot_rs {
         max_iter
     }
 
+    fn range(start: f64, stop: f64, count: usize) -> Vec<f64> {
+        let step = (stop - start) / count as f64;
+        (0..count)
+            .map(|n| (n as f64).mul_add(step, start))
+            .collect()
+    }
+
     #[pyfunction]
     fn calculate_mandelbrot(x_min: f64, x_max: f64, y_min: f64, y_max: f64, width: usize, height: usize, max_iter: u32) 
             -> Vec<Vec<u32>> {
-        let x_step = (x_max - x_min) / width as f64;
-        let y_step = (y_max - y_min) / height as f64;
+        let xrange = range(x_min, x_max, width);
+        let yrange = range(y_min, y_max, height);
 
         let mut result = vec![vec![0; width]; height];
 
         for y in 0..height {
-            let cy = (y as f64).mul_add(y_step, y_min);
+            let cy = yrange[y];
             for x in 0..width {
-                let cx = (x as f64).mul_add(x_step, x_min);
-
+                let cx = xrange[x];
                 result[y][x] = mandelbrot_iterate(cx, cy, max_iter);
             }
         }
